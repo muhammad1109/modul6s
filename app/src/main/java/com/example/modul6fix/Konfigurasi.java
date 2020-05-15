a4583a8e4dd649369902b980f3a788c91f135627
muhammad1109 <muhammadnurhidayat14045@gmail.com>
2020-05-15 13:20:05 +0700

    Maaf telat

---
+++ app/src/main/java/com/example/modul6fix/Konfigurasi.java
@@ -0,0 +1,23 @@
+package com.example.modul6fix;
+
+public class Konfigurasi {
+    public static final String URL_ADD = "https://tugasprogramkasir-100.000webhostapp.com/crudtest/insert.php";
+    public static final String URL_GET_ALL = "https://tugasprogramkasir-100.000webhostapp.com/crudtest/read.php";
+    public static final String URL_GET_MHS = "https://tugasprogramkasir-100.000webhostapp.com/crudtest/select.php";
+    public static final String URL_UPDATE_MHS = "https://tugasprogramkasir-100.000webhostapp.com/crudtest/update.php";
+    public static final String URL_DELETE_MHS = "https://tugasprogramkasir-100.000webhostapp.com/crudtest/delete.php";
+    //Dibawah ini merupakan Kunci yang akan digunakan untuk mengirim permintaan ke Script PHP
+    public static final String KEY_MHS_ID = "id";
+    public static final String KEY_MHS_NAMA = "nama";
+    public static final String KEY_MHS_JURUSAN = "jurusan";
+    public static final String KEY_MHS_EMAIL = "email";
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID = "id";
+    public static final String TAG_NAMA = "nama";
+    public static final String TAG_JURUSAN = "jurusan";
+    public static final String TAG_EMAIL = "email";
+    //ID Mahasiswa
+    //mhs itu singkatan dari Mahasiswa
+    public static final String MHS_ID = "mhs_id";
+}
